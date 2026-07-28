15606b9cddcdfe40b754b4ea6a301e0ea8cf57c0
Maksym Yurchenko <maksymyurchenko@Maksyms-MacBook-Pro.local>
2026-07-28 15:43:16 +0300

    email title font weight update

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,7 @@
 @props(['url'])
 <tr>
 <td class="header">
-<a href="{{ $url }}" style="display: inline-block; font-family: Georgia, Times, serif; font-size: 26px; letter-spacing: 3px; color: #000000; text-decoration: none;">
+<a href="{{ $url }}" style="display: inline-block; font-family: 'Cormorant Garamond', ui-serif, Georgia, Cambria, 'Times New Roman', Times, serif; font-size: 26px; font-weight: 300; letter-spacing: 3px; color: #000000; text-decoration: none;">
 HIRE COLLECTIVE
 </a>
 </td>
